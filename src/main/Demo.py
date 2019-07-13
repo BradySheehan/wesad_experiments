d1af2fcedf0e64835b3b6d6e5ee23d6c57234e61
Brady Sheehan <bradyasheehan@gmail.com>
2019-07-13 13:20:31 -0400

    Readme updates and removed __init__ from Demo.py

--- src/main/Demo.py
+++ src/main/Demo.py
@@ -15,35 +15,34 @@ from DataManager import DataManager
 
 class Demo:
     
-    def __init__(self):
-        manager = DataManager()
+    manager = DataManager()
+    
+    print("Preparing data for model creation..")
+    manager.load_all()
+    print("Considering baseline experiment data:")
+    manager.compute_features()
+    print("Considering stress experiment data:")
+    manager.compute_features_stress()
+    
+    batch_size = 2
+    epochs = 1
+    print("===============================================================")
+    
+    print("Creating the LSTM network with", epochs, "epochs.")
+    # compute for one epoch
+    (model, X_train, X_test, y_train, y_test) = \
+        manager.create_network(epochs, batch_size)
         
-        print("Preparing data for model creation..")
-        manager.load_all()
-        print("Considering baseline experiment data:")
-        manager.compute_features()
-        print("Considering stress experiment data:")
-        manager.compute_features_stress()
-        
-        batch_size = 2
-        epochs = 1
-        print("===============================================================")
-        
-        print("Creating the LSTM network with", epochs, "epochs.")
-        # compute for one epoch
-        (model, X_train, X_test, y_train, y_test) = \
-            manager.create_network(epochs, batch_size)
-            
-        print("===============================================================")
-        
-        print("Evaluating LSTM network with", epochs, "epochs.")
-        manager.get_model_results(model, X_train, X_test, y_train, y_test )
-        
-        
-        print("===============================================================")
-        
-        print("Loading and evaluating LSTM network from 5 epochs")
-        # then load a previously computed 5 epoch model and display the results
-        model_5_epochs = manager.load_model('model-2019-07-1310_40_14.h5')
-        manager.get_model_results(model_5_epochs, X_train, X_test, y_train, y_test )
+    print("===============================================================")
+    
+    print("Evaluating LSTM network with", epochs, "epochs.")
+    manager.get_model_results(model, X_train, X_test, y_train, y_test )
+    
+    
+    print("===============================================================")
+    
+    print("Loading and evaluating LSTM network from 5 epochs")
+    # then load a previously computed 5 epoch model and display the results
+    model_5_epochs = manager.load_model('model-2019-07-1310_40_14.h5')
+    manager.get_model_results(model_5_epochs, X_train, X_test, y_train, y_test )
 
