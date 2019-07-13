c5898dddb6c5b86417b4b6482c10d2ea6837389f
Brady Sheehan <bradyasheehan@gmail.com>
2019-07-13 13:07:25 -0400

    Slight name fix and updated path references for install locations

--- src/main/DataManager.py
+++ src/main/DataManager.py
@@ -27,8 +27,8 @@ from keras.models import load_model
 
 class DataManager:
     # Path to the WESAD dataset
-    #ROOT_PATH = '/media/learner/6663-3462/WESAD/'
-    ROOT_PATH = r'C:\WESAD'
+    ROOT_PATH = '/media/learner/6663-3462/WESAD/'
+    #ROOT_PATH = r'C:\WESAD'
     
     # pickle file extension for importing
     FILE_EXT = '.pkl'
@@ -87,6 +87,7 @@ class DataManager:
         if os.path.isfile(path):
             return path
         else:
+            print(path)
             raise Exception('Invalid subject: ' + str(subject))
 
     def load(self, subject):
